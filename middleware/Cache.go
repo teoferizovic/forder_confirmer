2cad8fb664f4209ec0596e87058a11fbb24cdd12
teoferizovic <ferizovic.teo@gmail.com>
2019-01-24 15:40:38 +0100

    In cache middleware unmarshal data from Redis to interface , instead in struct (reason:now can be dinamix unmarshal).

--- middleware/Cache.go
+++ middleware/Cache.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"forder_confirmer/database"
-	"forder_confirmer/model"
+	/*"forder_confirmer/model"*/
 	"net/http"
 )
 
@@ -13,27 +13,37 @@ func  CacheMiddleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		datas, err := database.RedisConn2().Get(r.URL.String()).Result()
+		if r.Method=="GET" {
+			datas, err := database.RedisConn2().Get(r.URL.String()).Result()
 
-		if err != nil {
-			next.ServeHTTP(w, r)
-			fmt.Println("aaaa")
-		} else {
-			fmt.Println("bbbb")
-			bytes := []byte(datas)
-
-			var orders []model.FOrderR
-
-			err = json.Unmarshal(bytes, &orders)
 			if err != nil {
-				fmt.Println(err)
+				next.ServeHTTP(w, r)
+				fmt.Println("aaaa")
+			} else {
+				fmt.Println("bbbb")
+				bytes := []byte(datas)
+
+				/*var orders []model.FOrderR
+
+				err = json.Unmarshal(bytes, &orders)
+				if err != nil {
+					fmt.Println(err)
+				}*/
+
+				var raw interface{}
+				err = json.Unmarshal(bytes, &raw)
+
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(raw)
+				//next.ServeHTTP(w, r)
+				return
 			}
-			//fmt.Println(ps)
-			//fmt.Println(datas)
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(orders)
-			//next.ServeHTTP(w, r)
-			return
+		} else {
+			next.ServeHTTP(w, r)
 		}
 	})
 
